0d73f6cf3183994c6fd7c166f77a3877dfa85cd2
Diogo Souza <diogo.souza@msn.com>
2020-06-15 22:02:22 -0400

    correções no docker

--- api/gulpfile.js
+++ api/gulpfile.js
@@ -2,7 +2,6 @@ const { series, src, dest } = require('gulp')
 const del = require('delete')
 const babel = require('gulp-babel')
 const terser = require('gulp-terser')
-const rename = require('gulp-rename')
 
 function clean(cb) {
   del(['dist/'], cb);
@@ -12,9 +11,6 @@ function build() {
   return src('src/*.js')
     .pipe(babel())
     .pipe(terser())
-    .pipe(rename({ extname: '.min.js' }))
-    .pipe(dest('dist/'))
-    .pipe(src('db.json'))
     .pipe(dest('dist/'));
 }
 
